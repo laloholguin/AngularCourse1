0ac653d18f1cc95b4613de7e574a4c0b60ec9fe7
laloholguin <eholguin.-@hotmail.com>
2016-09-01 11:59:03 -0600

    ng-show, ng-init, behavior tabs

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,21 @@
 (function(){
 	var gem={ 
-		name: 'Azulita',
-		price: 2.95};
-		description:'Lorem ipsum dolor sit amet'
+		name: 'Azurite',
+		price: 2.95,
+		description:'Lorem ipsum dolor sit amet',
+		canPurchase: false,
+		soldOut: true
 	};
+
+	var gems = [
+	{name: 'Azurite', price: 2.95},
+	{name: 'Bloodstone', price: 5.95},
+	{name: 'Zircon', price: 3.95}
+	];
+	
 	var app = angular.module('gemStore', []);
 
-	app.controller('StoreController'), function(){
-		this.product = gem;
-
+	app.controller('StoreController', function(){
+		this.products = gems;
 	});
 })();
